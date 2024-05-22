5cb438bbb6c3f929d22502c1c4fbaffa8efce43a
leo gardarein <leo.gardarein@epita.fr>
2024-05-22 18:28:29 +0200

    a

---
+++ Db_bluedit/dbo/Tables/user.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[user] (
+    [id]           BIGINT        IDENTITY (1, 1) NOT NULL,
+    [name]         VARCHAR (32)  NOT NULL,
+    [password]     VARCHAR (32)  NOT NULL,
+    [description]  VARCHAR (256) NOT NULL,
+    [creationDate] DATETIME      CONSTRAINT [DF_user_creationDate] DEFAULT (getdate()) NOT NULL,
+    [mail]         VARCHAR (64)  NOT NULL,
+    CONSTRAINT [PK_user] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+
+
