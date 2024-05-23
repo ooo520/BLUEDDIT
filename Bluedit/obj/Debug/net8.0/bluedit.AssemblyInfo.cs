e8ce60de7f5eaa2806162eb5f94ed20f99d53774
Dylan <dylan.toledano@tutanota.com>
2024-05-23 17:13:34 +0200

    trying

--- Bluedit/obj/Debug/net8.0/bluedit.AssemblyInfo.cs
+++ Bluedit/obj/Debug/net8.0/bluedit.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bluedit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ec6f5f7a9025547d4ab7a669fdc57f15a602743")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03363f7a58ea037246b83b026aaf68e5d8f05ff")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("Bluedit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bluedit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
