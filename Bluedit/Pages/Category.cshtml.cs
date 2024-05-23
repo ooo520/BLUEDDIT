eab22fae71a3dcfdef902af51849686691b6239a
Dylan <dylan.toledano@tutanota.com>
2024-05-23 17:37:32 +0200

    Remove those pesky trash files AND DO NOT EVER ADD THEM AGAIN PLEASE THANK YOU

---
+++ Bluedit/Pages/Category.cshtml.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+
+namespace Bluedit.Pages
+{
+
+	public class CategoryModel : PageModel
+	{
+		public List<Dbo.Thread> Threads { get; set; }
+		[BindProperty(SupportsGet = true)]
+		public string? CategoryName { get; set; }
+
+		private readonly DataAccess.Interfaces.IThreadRepository _threadRepository;
+
+		public CategoryModel(DataAccess.Interfaces.IThreadRepository threadRepository)
+		{
+			_threadRepository = threadRepository;
+		}
+
+		public async Task<IActionResult> OnGetAsync()
+        {
+			if (CategoryName == null)
+			{
+				return NotFound();
+			}
+
+			Debug.WriteLine(CategoryName);
+
+			// TODO: pagination ?
+			Threads = (await _threadRepository.Read()).ToList();
+
+			return Page();
+		}
+    }
+}
