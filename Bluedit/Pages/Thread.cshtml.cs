3f99b27c090b0f78d488653e239f2539f313cb36
Dylan <dylan.toledano@tutanota.com>
2024-05-24 15:30:16 +0200

    feat: add Thread page

---
+++ Bluedit/Pages/Thread.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bluedit.Pages
+{
+    public class ThreadModel : PageModel
+    {
+        public ICollection<Dbo.Answer> Answers { get; set; }
+
+		[BindProperty(SupportsGet = true)]
+		public string? CategoryName { get; set; }
+		[BindProperty]
+		public long ThreadId { get; set; }
+		public Dbo.Thread? Thread { get; set; }
+
+		private readonly DataAccess.Interfaces.IThreadRepository _threadRepository;
+		private readonly DataAccess.Interfaces.IAnswerRepository _answerRepository;
+
+		public ThreadModel(
+			DataAccess.Interfaces.IThreadRepository threadRepository,
+			DataAccess.Interfaces.IAnswerRepository answerRepository
+		)
+		{
+			_threadRepository = threadRepository;
+			_answerRepository = answerRepository;
+		}
+
+		public async Task<IActionResult> OnGetAsync()
+        {
+			if (ThreadId == null)
+			{
+				return NotFound();
+			}
+			Thread = _threadRepository.GetById(ThreadId);
+			Answers = _answerRepository.GetByThread(ThreadId);
+			return Page();
+        }
+    }
+}
