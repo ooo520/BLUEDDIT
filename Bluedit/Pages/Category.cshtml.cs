2910af9268ed2b5e9fb12fd6186ee9079047af02
Dylan <dylan.toledano@tutanota.com>
2024-05-24 11:56:42 +0200

    feat(Category.cshtml): better Category page

--- Bluedit/Pages/Category.cshtml.cs
+++ Bluedit/Pages/Category.cshtml.cs
@@ -1,37 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Diagnostics;
 
 namespace Bluedit.Pages
 {
 
 	public class CategoryModel : PageModel
 	{
-		public List<Dbo.Thread> Threads { get; set; }
+		public ICollection<Dbo.Thread> Threads { get; set; }
 		[BindProperty(SupportsGet = true)]
 		public string? CategoryName { get; set; }
+		public Dbo.Category? Category { get; set; }
 
 		private readonly DataAccess.Interfaces.IThreadRepository _threadRepository;
+		private readonly DataAccess.Interfaces.ICategoryRepository _categoryRepository;
 
-		public CategoryModel(DataAccess.Interfaces.IThreadRepository threadRepository)
+		public CategoryModel(
+			DataAccess.Interfaces.IThreadRepository threadRepository,
+			DataAccess.Interfaces.ICategoryRepository categoryRepository
+		)
 		{
 			_threadRepository = threadRepository;
+			_categoryRepository = categoryRepository;
 		}
 
 		public async Task<IActionResult> OnGetAsync()
-        {
+		{
 			if (CategoryName == null)
 			{
 				return NotFound();
 			}
 
-			Debug.WriteLine(CategoryName);
+			Category = _categoryRepository.GetByName(CategoryName);
+			if (Category == null)
+			{
+				return NotFound();
+			}
 
 			// TODO: pagination ?
-			//Threads = (await _threadRepository.Read()).ToList();
-			Threads = [];
+			Threads = _threadRepository.GetByCategory(Category.Id, null);
 
 			return Page();
 		}
-    }
+	}
 }
