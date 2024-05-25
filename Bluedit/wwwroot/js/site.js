02b1f46ac632c45c2d0d5c070179e34de1a54aa6
Dylan <dylan.toledano@tutanota.com>
2024-05-25 19:59:12 +0200

    feat(Thread.cshtml): display opinions

--- Bluedit/wwwroot/js/site.js
+++ Bluedit/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function submitOpinion(kind, answerId) {
+	const xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function () {
+			if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+		console.info("Opinion submitted successfully");
+			}
+		}
+	xmlHttp.open("POST", `/api/opinion/${kind}/${answerId}`, true); // true for asynchronous
+	xmlHttp.send(null);
+}
