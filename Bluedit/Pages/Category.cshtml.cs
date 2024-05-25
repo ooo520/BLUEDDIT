0d20d97d32048825ac18345c1daa1047454c4e16
leo gardarein <leo.gardarein@epita.fr>
2024-05-25 20:37:20 +0200

    feat(sort and serch) : now on get method

--- Bluedit/Pages/Category.cshtml.cs
+++ Bluedit/Pages/Category.cshtml.cs
@@ -17,7 +17,23 @@ namespace bluedit.Pages
 
 		public Dictionary<long, Dbo.Answer> ThreadIdToRootAnswerMap { get; set; } = new();
 
-		public readonly DataAccess.Interfaces.IAnswerRepository _answerRepository;
+        public List<SelectListItem> SortOptions { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "ASC", Text = "Nouveaux" },
+            new SelectListItem { Value = "DESC", Text = "Ancien" },
+            new SelectListItem { Value = "TOP", Text = "Populaire" },
+
+            new SelectListItem { Value = "A-Z", Text = "A à Z" },
+            new SelectListItem { Value = "Z-A", Text = "Z à A" },
+        };
+
+        [BindProperty(SupportsGet = true)]
+        public string Sorting { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchQuery { get; set; }
+
+        public readonly DataAccess.Interfaces.IAnswerRepository _answerRepository;
 		public readonly DataAccess.Interfaces.ICategoryRepository _categoryRepository;
 		public readonly DataAccess.Interfaces.IThreadRepository _threadRepository;
 		public readonly DataAccess.Interfaces.IOpinionRepository _opinionRepository;
@@ -36,84 +52,49 @@ namespace bluedit.Pages
 			_opinionRepository = opinionRepository;
 			_userRepository = userRepository;
 		}
-		void initPage()
+
+        public IActionResult OnGet()
 		{
-			if (CategoryName == null) return;
-			Category = _categoryRepository.GetByName(CategoryName);
-			if (Category == null) return;
-			Threads = _threadRepository.GetByCategory(Category.Id, null);
-			if (Threads == null) return;
-			foreach (Dbo.Thread thread in Threads)
-			{
-				Dbo.Answer rootAnswer = _answerRepository.GetRootAnswerOfThread(thread.Id);
-				if (rootAnswer == null)
-				{
-					throw new NullReferenceException($"GetRootAnswerOfThread returned null for thread with id {thread.Id}.");
-				}
-				ThreadIdToRootAnswerMap.Add(thread.Id, rootAnswer);
-			}
-		}
+            if (CategoryName == null) return NotFound();
+            Category = _categoryRepository.GetByName(CategoryName);
+            if (Category == null) return NotFound();
+            Threads = _threadRepository.GetByCategory(Category.Id, null);
 
-
-		public IActionResult OnGet()
-		{
-			initPage();
-			if (CategoryName == null || Category == null)
-			{
-				return NotFound();
-			}
-			return Page();
-		}
-
-		public List<SelectListItem> SortOptions { get; } = new List<SelectListItem>
-		{
-			new SelectListItem { Value = "ASC", Text = "Nouveaux" },
-			new SelectListItem { Value = "DESC", Text = "Ancien" },
-			new SelectListItem { Value = "TOP", Text = "Populaire" },
-
-			new SelectListItem { Value = "A-Z", Text = "A à Z" },
-			new SelectListItem { Value = "Z-A", Text = "Z à A" },
-		};
-		[BindProperty]
-		public string Sorting { get; set; }
-
-		[BindProperty]
-		public string SearchQuery { get; set; }
-		public IActionResult OnPost()
-		{
-			initPage();
-
-			Console.WriteLine(Threads);
-			Console.WriteLine(Sorting);
-			Console.WriteLine(SearchQuery);
-			if (Threads != null)
-			{
-				
-				if (!string.IsNullOrEmpty(SearchQuery))
-					Threads = Threads.Where((t) => t.Title.Contains(SearchQuery)).ToList();
-				switch (Sorting)
-				{
-					case "ASC":
-						Threads = Threads.OrderBy((t) => ThreadIdToRootAnswerMap[t.Id].CreationDate).ToList();
-						break;
-					case "DESC":
-						Threads = Threads.OrderByDescending((t) => ThreadIdToRootAnswerMap[t.Id].CreationDate).ToList();
-						break;
-					case "A-Z":
-						Threads = Threads.OrderBy((t) => t.Title).ToList();
-						break;
-					case "Z-A":
-						Threads = Threads.OrderByDescending((t) => t.Title).ToList();
-						break;
-					case "TOP":
-						Threads = Threads.OrderBy((t) => _opinionRepository.GetLikesCountForAnswer(ThreadIdToRootAnswerMap[t.Id].Id)).ToList();
-						break;
-					default:
-						Threads = Threads.OrderBy((t) => ThreadIdToRootAnswerMap[t.Id].CreationDate).ToList();
-						break;
-				}
-			}
-			return Page();
+            foreach (Dbo.Thread thread in Threads)
+            {
+                Dbo.Answer rootAnswer = _answerRepository.GetRootAnswerOfThread(thread.Id);
+                if (rootAnswer == null)
+                {
+                    throw new NullReferenceException($"GetRootAnswerOfThread returned null for thread with id {thread.Id}.");
+                }
+                ThreadIdToRootAnswerMap.Add(thread.Id, rootAnswer);
+            }
+            if (Threads != null)
+            {
+                if (!string.IsNullOrEmpty(SearchQuery))
+                    Threads = Threads.Where((t) => t.Title.Contains(SearchQuery)).ToList();
+                switch (Sorting)
+                {
+                    case "ASC":
+                        Threads = Threads.OrderBy((t) => ThreadIdToRootAnswerMap[t.Id].CreationDate).ToList();
+                        break;
+                    case "DESC":
+                        Threads = Threads.OrderByDescending((t) => ThreadIdToRootAnswerMap[t.Id].CreationDate).ToList();
+                        break;
+                    case "A-Z":
+                        Threads = Threads.OrderBy((t) => t.Title).ToList();
+                        break;
+                    case "Z-A":
+                        Threads = Threads.OrderByDescending((t) => t.Title).ToList();
+                        break;
+                    case "TOP":
+                        Threads = Threads.OrderBy((t) => _opinionRepository.GetLikesCountForAnswer(ThreadIdToRootAnswerMap[t.Id].Id)).ToList();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return Page();
 		}
 	}
 }
