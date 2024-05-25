a94411e50840853c13a2f6a3c9f8826b2b6aec93
quentinfeng <quentin.feng@epita.fr>
2024-05-25 20:39:44 +0200

    feat(profile):
    - add update description
    - prevent unauthorized access to profile

--- Bluedit/Pages/Profile.cshtml.cs
+++ Bluedit/Pages/Profile.cshtml.cs
@@ -1,20 +1,26 @@
 using bluedit.DataAccess.Interfaces;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Web;
 
 namespace bluedit.Pages
 {
     public class ProfileModel : PageModel
     {
-        private IUserRepository _userRepository { get; set; }
+        private readonly IUserRepository _userRepository;
+		private readonly IHttpContextAccessor _httpContextAccessor;
 		[BindProperty(SupportsGet = true)]
         public string? UserName { get; set; }
         public Dbo.User Blueditor {  get; set; }
-        public ProfileModel(IUserRepository userRepository)
+
+        public ProfileModel(IUserRepository userRepository, IHttpContextAccessor httpContextAccessor)
         {
             _userRepository = userRepository;
+            _httpContextAccessor = httpContextAccessor;
         }
-        public async Task<IActionResult> OnGetAsync()
+
+        public IActionResult OnGet()
         {
             if (UserName == null)
             {
@@ -28,16 +34,54 @@ namespace bluedit.Pages
                 return NotFound();
             }
 
-            Blueditor = user;
+			if (user.Name != _httpContextAccessor.HttpContext.Request.Cookies["username"]
+                || user.Password != _httpContextAccessor.HttpContext.Request.Cookies["userpass"])
+            {
+                return Unauthorized();  // using unauthorized instead of Forbid() because it would show unhandled exception otherwise
+            }
+
+			Blueditor = user;
 
             return Page();
         }
+
+        public async Task<IActionResult> OnPostSaveAsync()
+        {
+			if (UserName == null)
+			{
+				return BadRequest();
+			}
+
+			Dbo.User user = _userRepository.GetByName(UserName);
+
+			if (user == null)
+			{
+				return NotFound();
+			}
+
+			if (user.Name != _httpContextAccessor.HttpContext.Request.Cookies["username"]
+				|| user.Password != _httpContextAccessor.HttpContext.Request.Cookies["userpass"])
+			{
+				return Unauthorized();  // using unauthorized instead of Forbid() because it would show unhandled exception otherwise
+			}
+
+			var newDescription = Request.Form["Description"];
+            if (newDescription.ToString().Length > 256)
+            {
+                return BadRequest();
+            }
+
+            user.Description = newDescription;
+            await _userRepository.Update(user);
+
+            return Redirect(_httpContextAccessor.HttpContext.Request.GetEncodedPathAndQuery());
+		}
+
         public IActionResult OnPostDisconnect()
         {
-            //Console.WriteLine("aaaaaaaaaaaaaa");
             HttpContext.Response.Cookies.Delete("username");
 			HttpContext.Response.Cookies.Delete("userpass");
-			return RedirectToPage("/");
+			return RedirectToPage("/Index");
         }
     }
 }
